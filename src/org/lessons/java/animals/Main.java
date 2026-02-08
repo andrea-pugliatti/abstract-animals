61d7c1e2562b73fe65a64cb44ed0847eac3f26b2
andrea-pugliatti <andrea.pugliatti@outlook.it>
2026-02-08 18:32:00 +0100

    add Main

---
+++ src/org/lessons/java/animals/Main.java
@@ -0,0 +1,15 @@
+package org.lessons.java.animals;
+
+public class Main {
+    public static void main(String[] args) {
+        Aquila aquila = new Aquila();
+        Cane cane = new Cane();
+        Passerotto passerotto = new Passerotto();
+        Delfino delfino = new Delfino();
+
+        aquila.dormi();
+        cane.verso();
+        passerotto.mangia();
+        delfino.verso();
+    }
+}
