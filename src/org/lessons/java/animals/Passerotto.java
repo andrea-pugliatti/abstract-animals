9d1d2afb73964bfa00df0dcaf26d9448ad3c25b4
andrea-pugliatti <andrea.pugliatti@outlook.it>
2026-02-08 18:25:59 +0100

    add Passerotto class

---
+++ src/org/lessons/java/animals/Passerotto.java
@@ -0,0 +1,12 @@
+package org.lessons.java.animals;
+
+public class Passerotto extends AbstractAnimale {
+    public void verso() {
+        System.out.println("Chirp chirp");
+    }
+
+    public void mangia() {
+        System.out.println("Il passerotto mangia insetti e semi");
+
+    }
+}
