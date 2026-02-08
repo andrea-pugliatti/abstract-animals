385a7105367baca796fb51a0605e47a8a1c11421
andrea-pugliatti <andrea.pugliatti@outlook.it>
2026-02-08 18:28:00 +0100

    add Aquila class

---
+++ src/org/lessons/java/animals/Aquila.java
@@ -0,0 +1,12 @@
+package org.lessons.java.animals;
+
+public class Aquila {
+    public void verso() {
+        System.out.println("Keee keee");
+    }
+
+    public void mangia() {
+        System.out.println("L'aquila mangia un roditore");
+
+    }
+}
