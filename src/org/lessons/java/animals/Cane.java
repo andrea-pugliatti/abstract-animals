f09f24f0fcf4fc99f87a6ac0a8b5c1ad2bcdc589
andrea-pugliatti <andrea.pugliatti@outlook.it>
2026-02-08 18:23:41 +0100

    Add Cane class

---
+++ src/org/lessons/java/animals/Cane.java
@@ -0,0 +1,12 @@
+package org.lessons.java.animals;
+
+public class Cane extends AbstractAnimale {
+    public void verso() {
+        System.out.println("Bau bau");
+    }
+
+    public void mangia() {
+        System.out.println("Il cane mangia croccantini");
+
+    }
+}
